9d2f64eed195418707386c1539c774cc1f5e5a5b
Mr-emeka <speak2c.emeka@gmail.com>
2019-10-13 18:53:45 +0100

    fixed about error

--- about.php
+++ about.php
@@ -1,66 +1,3 @@
-<? php
-$servername = "sql12.freemysqlhosting.net";
-$database = "sql12308286"; 
-$username = "sql12308286";
-$password = "ZwVT4iplj3";
-$sql = "mysql:host=$servername;dbname=$database;";
-$dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
-
-
-// Create a new connection to the MySQL database using PDO, $my_Db_Connection is an object
-try { 
-  $my_Db_Connection = new PDO($sql, $username, $password, $dsn_Options);
-  echo "Connected successfully";
-} catch (PDOException $error) {
-  echo 'Connection error: ' . $error->getMessage();
-}
-// Processing form data when form is submitted
-if(isset($_POST['submit'])){
-
-    $name = trim($_POST["name"]);
-    $email = trim($_POST["email"]);
-    $phone = trim($_POST["number"]);
-    $message = trim($_POST["message"]);
-
-if(empty($name)){
-   $error='name field cant be empty';
-}
-elseif (empty($email)){
-    $error='Mail field Cant be empty';
-}
-elseif(empty($phone)){
-   $error='Phone Field cant be empty';
-}
-elseif (empty($message)){
-    $error='message field cant be empty';
-}
-elseif (!filter_var($email,FILTER_VALIDATE_EMAIL)){
-    $error='Invalid Email Address';
-}
-else {
-
-    // Here we create a variable that calls the prepare() method of the database object
-    // The SQL query you want to run is entered as the parameter, and placeholders are written like this :placeholder_name
-    $my_Insert_Statement = $my_Db_Connection->prepare("INSERT INTO contact (`name`,`email`,`phone`,`message`) VALUES (?,?,?)");
-    
-    // Now we tell the script which variable each placeholder actually refers to using the bindParam() method
-    // First parameter is the placeholder in the statement above - the second parameter is a variable that it should refer to
-    $my_Insert_Statement->bind_param('ssis',$name,$email,$phone,$message); 
-    // $my_Insert_Statement->bindParam(:full_name, $name);
-    // $my_Insert_Statement->bindParam(:email, $email);
-    // $my_Insert_Statement->bindParam(:phone, $phone);
-    // $my_Insert_Statement->bindParam(:message, $message);
-    
-    // Execute the query using the data we just defined
-    // The execute() method returns TRUE if it is successful and FALSE if it is not, allowing you to write your own messages here
-    if ($my_Insert_Statement->execute()) {
-      $message = "message sent successfully";
-    } else {
-     $error ="Something went wrong. Please try again later.";
-    }
-
-}
-?>
 <!DOCTYPE html>
 <html>
 
@@ -134,19 +71,9 @@ else {
 
                 <div class="col-md-6">
                     <h2 class="pt-3">Contact Us</h2>
-                    <?php
-                    if(!empty($messages)){
-                        echo "<div  class='alert-success'>";
-                        echo $messages;
-                        echo "</div>";
-                    }
-                    if(!empty($error)){
-                        echo "<div  class='alert-danger'>";
-                        echo $error;
-                        echo "</div>";
-                    }
-                    ?>
-                    <form action="<?=$_SERVER['PHP_SELF']?>" method="POST" id="form-contact">
+              <div class="form-messages">
+              </div>
+            <form action="contact.php" method="POST" id="form-contact">
                         <input type="text" class='field' name="name" placeholder="Your Name" required>
                         <input type="email" class="field" name="email" placeholder="Email Address" required>
                         <input type="text" class="field" name="number" placeholder="Telephone Number" required>
